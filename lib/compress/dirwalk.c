d004fbb610f6bbca8f43daf9081491235f919c8d
neo2043 <prabhatkumar2043@gmail.com>
2024-09-10 18:10:44 +0530

    fixed arraylist, dirwalk

--- lib/compress/dirwalk.c
+++ lib/compress/dirwalk.c
@@ -35,7 +35,9 @@ char *walk_get_absolute_path(walk_ctx *pathCtx, int index, int only_relative_pat
             only_relative_path = 0;
             continue;
         }
-        if (index != -1 && ctx.index > index) { continue; }
+        if (index != -1 && ctx.index > index) {
+            continue;
+        }
         int pathlen = ((strlen(((segment_t *)ctx.item)->segment_name) + 1) + (strlen(path) + 1));
         path = realloc(path, sizeof(char) * pathlen);
         cwk_path_join(path, ((segment_t *)ctx.item)->segment_name, path, pathlen);
@@ -43,7 +45,7 @@ char *walk_get_absolute_path(walk_ctx *pathCtx, int index, int only_relative_pat
     return path;
 }
 
-void add_new_seg_in_arr(walk_ctx *ctx, char *segment_name, long associated_no) {
+static void add_new_seg_in_arr(walk_ctx *ctx, char *segment_name, long associated_no) {
     segment_t *new_seg = calloc(1, sizeof(segment_t));
     new_seg->segment_name = calloc(strlen(segment_name) + 1, sizeof(char));
     strcpy(new_seg->segment_name, segment_name);
@@ -91,6 +93,11 @@ walk_ctx init_walk_ctx(char *root_path) {
     return ctx;
 }
 
+static void deinit_walk_segment(segment_t *temp) {
+    free(temp->segment_name);
+    free(temp);
+}
+
 void deinit_walk_ctx(walk_ctx *ctx) {
     free(ctx->stbuf);
     for (int i = 0; i < arraylist_size(ctx->path_segment_stack); i++) {
@@ -100,12 +107,7 @@ void deinit_walk_ctx(walk_ctx *ctx) {
     arraylist_destroy(ctx->path_segment_stack);
 }
 
-void deinit_walk_segment(segment_t *temp) {
-    free(temp->segment_name);
-    free(temp);
-}
-
-struct dirent *readdir_handler(DIR *dir) {
+static struct dirent *readdir_handler(DIR *dir) {
     struct dirent *de;
     do {
         de = readdir(dir);
@@ -116,6 +118,7 @@ struct dirent *readdir_handler(DIR *dir) {
 
 int walk_next(walk_ctx *ctx) {
     segment_t *lst_seg = arraylist_get(ctx->path_segment_stack, arraylist_size(ctx->path_segment_stack) - 1);
+    int freed = 0;
     if (lst_seg->seg_type == folder) {
         DIR *dir;
         if (lst_seg->passed_over) {
@@ -126,13 +129,16 @@ int walk_next(walk_ctx *ctx) {
             seekdir(dir, lst_seg->associated_no);
             segment_t *temp = arraylist_pop(ctx->path_segment_stack);
             deinit_walk_segment(temp);
+            freed = 1;
         } else {
             char *abs_path = walk_get_absolute_path(ctx, -1, 0);
             dir = opendir(abs_path);
             free(abs_path);
         }
         struct dirent *de;
-        lst_seg->passed_over = true;
+        if(!freed){
+            lst_seg->passed_over = true;
+        }
         if ((de = readdir_handler(dir)) == NULL) {
             closedir(dir);
             return walk_next(ctx);
